b2245f7a082f4f7794763545d06b9372200c73eb
Ophus Wong <ophus.wong@gmail.com>
2018-01-31 19:54:35 -0700

    Write helper tests for vehicle and planet data Add cleanPlanetData, getPlanetResidents, cleanVehicleData

--- src/components/helper/helper.js
+++ src/components/helper/helper.js
@@ -2,8 +2,8 @@ export const cleanFilmData = (filmData) => {
   return filmData.results.map(film => {
     return {
       title: film.title,
-      opening_crawl: film.opening_crawl,
-      release_date: film.release_date
+      crawl: film.opening_crawl,
+      releaseDate: film.release_date,
     }
   })
 }
@@ -14,7 +14,7 @@ const getHomeworldData = async (url) => {
 
   return {
     homeworld: homeworldObj.name,
-    population: homeworldObj.population
+    population: homeworldObj.population,
   }
 }
 
@@ -25,7 +25,6 @@ const getSpeciesData = (urls) => {
 
     return species.name
   })
-
   return Promise.all(unresolvedPromises);
 }
 
@@ -34,14 +33,49 @@ export const cleanPeopleData = (peopleData) => {
     const homeworld = await getHomeworldData(person.homeworld)
     const speciesTypes = await getSpeciesData(person.species)
 
-    const thing = {
+    return {
       name: person.name,
       ...homeworld,
-      species: speciesTypes
+      species: speciesTypes,
     }
+  })
+  return Promise.all(people);
+}
 
-    return thing;
+export const cleanVehicleData = (vehicleData) => {
+  return vehicleData.results.map((vehicle) => {
+    return {
+      name: vehicle.name,
+      model: vehicle.model,
+      passengers: vehicle.passengers,
+      vehicleClass: vehicle.vehicle_class,
+    }
+  })
+}
+
+export const getPlanetResidents = (urls) => {
+  const unresolvedPromises = urls.map( async (url) => {
+    const initialFetch = await fetch(url)
+    const resident = await initialFetch.json()
+
+    return resident.name
   })
 
-  return Promise.all(people);
+  return Promise.all(unresolvedPromises)
+}
+
+export const cleanPlanetData = (planetData) => {
+  const planets = planetData.results.map( async (planet) => {
+    const residents = await getPlanetResidents(planet.residents)
+
+    return {
+      name: planet.name,
+      climate: planet.climate,
+      terrain: planet.terrain,
+      population: planet.population,
+      residents: residents,
+    }
+  })
+
+  return Promise.all(planets)
 }
--- src/components/helper/helper.test.js
+++ src/components/helper/helper.test.js
@@ -1,28 +1,59 @@
-import {cleanFilmData, cleanPeopleData} from './helper.js';
+import {
+  cleanFilmData,
+  cleanPeopleData,
+  cleanVehicleData,
+  cleanPlanetData,
+  getPlanetResidents,
+} from './helper.js';
+
 import {
   mockFilmData,
   mockCleanFilmData,
   mockPeopleData,
   mockCleanPeopleData,
   mockPlanetData,
-  // mockCleanPlanetData,
+  mockCleanPlanetData,
   mockVehicleData,
-  // mockCleanVehicleData
+  mockCleanVehicleData
 } from '../mock-data';
 
 describe('HELPER', () => {
-  it('makes a clean array of film data given to it', () => {
+  it('cleans the array of film data given to it', () => {
     const result = cleanFilmData(mockFilmData)
 
     expect(result).toEqual(mockCleanFilmData)
   })
 
-  it.skip('makes a clean array of people data given to it', () => {
-    // const result = fetch(`https://swapi.co/api/people/`)
-    //   .then(response => response.json())
-    //   .then(result => cleanPeopleData(result))
+  it.skip('cleans the array of people data given to it', () => {
     const result = cleanPeopleData(mockPeopleData)
 
     expect(result).toEqual(mockCleanPeopleData)
+  })
+
+  it('cleans the array of vehicle data given to it', () => {
+    const result = cleanVehicleData(mockVehicleData)
+
+    expect(result).toEqual(mockCleanVehicleData)
+  })
+
+  it.skip('cleans the array of planet data given to it', () => {
+    const result = cleanPlanetData(mockPlanetData)
+
+    expect(result).resolves.toEqual(mockCleanPlanetData)
+  })
+
+  it('returns a residents array when status code is OK', () => {
+    const mockUrls = [
+      "https://swapi.co/api/people/5/",
+      "https://swapi.co/api/people/68/",
+      "https://swapi.co/api/people/81/"
+    ]
+    const mockResidents = [
+      "Leia Organa",
+      "Bail Prestor Organa",
+      "Raymus Antilles"
+    ]
+
+    expect(getPlanetResidents(mockUrls)).resolves.toEqual(mockResidents)
   })
 })
