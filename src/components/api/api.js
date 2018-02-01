33a493a6f656a61a1b00cc26a9dc4ba047e44342
Ophus Wong <ophus.wong@gmail.com>
2018-01-31 22:51:21 -0700

    Refactor getData method from App into apiGet

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,3 +1,33 @@
-export const apiGet = () => {
+import {
+  cleanFilmData,
+  cleanPeopleData,
+  cleanVehicleData,
+  cleanPlanetData
+} from '../helper/helper.js';
 
+const apiGet = async (request) => {
+  const url = `https://swapi.co/api/${request}/`
+  const initialFetch = await fetch(url)
+  const response = await initialFetch.json()
+  let data
+
+  switch(request) {
+    case 'films':
+      data = await cleanFilmData(response)
+      break;
+    case 'people':
+      data = await cleanPeopleData(response)
+      break;
+    case 'planets':
+      data = await cleanPlanetData(response)
+      break;
+    case 'vehicles':
+      data = await cleanVehicleData(response)
+      break;
+    default:
+      //Error goes here
+  }
+  return data
 }
+
+export default apiGet;
