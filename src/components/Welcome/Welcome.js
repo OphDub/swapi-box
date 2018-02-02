0890bcfd7d455d43cf9611923c2df1908e041d05
Ophus Wong <ophus.wong@gmail.com>
2018-02-02 15:08:29 -0700

    Add classes to welcome Add styling to Welcome

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Welcome.scss';
 
 const Welcome = ({ film }) => {
   return(
-    <article className="opening-crawl">
-      <h4 className="crawl-body">{film.crawl}</h4>
-      <h3 className="crawl-title">{film.title}</h3>
-      <h4 className="crawl-release-date">{film.releaseDate}</h4>
-    </article>
+  <div className="fade">
+    <section className="star-wars">
+      <article className="crawl">
+        <h4 className="crawl-body">{film.crawl}</h4>
+        <h3 className="crawl-title">{film.title}</h3>
+        <h4 className="crawl-release-date">{film.releaseDate}</h4>
+      </article>
+    </section>
+  </div>
   )
 }
 
