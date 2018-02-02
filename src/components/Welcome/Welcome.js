464bf61789820f079a45e45dfcbb91ee5fab6872
Ophus Wong <ophus.wong@gmail.com>
2018-02-02 13:55:04 -0700

    Add classNames to Welcome

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const Welcome = ({ film }) => {
-  console.log('welcome', film)
   return(
     <article className="opening-crawl">
-      <h4>{film.crawl}</h4>
-      <h3>{film.title}</h3>
-      <h4>{film.releaseDate}</h4>
+      <h4 className="crawl-body">{film.crawl}</h4>
+      <h3 className="crawl-title">{film.title}</h3>
+      <h4 className="crawl-release-date">{film.releaseDate}</h4>
     </article>
   )
 }
