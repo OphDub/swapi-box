37bbd12ab2b034cba9f13facd62480496eaf92f8
Ophus Wong <ophus.wong@gmail.com>
2018-03-23 17:39:53 -0600

    Finalize styling for dropdown responsive menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,21 +6,21 @@ import { func } from 'prop-types';
 const Nav = ({ getData }) => {
   return (
     <section className="nav-section">
-      <label for="reveal-email" class="btn"> Menu </label>
+      <label htmlFor="reveal-email" className="btn"> Menu </label>
       <input type="checkbox" id="reveal-email" role="button"></input>
       <nav id="nav-category" className="categories">
         <NavLink to='/people' className="nav-category-links">
-          <button onClick={() => getData('people')}>
+          <button onClick={() => getData('people')} className="nav-category-btns">
             People
           </button>
         </NavLink>
         <NavLink to='/planets' className="nav-category-links">
-          <button onClick={() => getData('planets')}>
+          <button onClick={() => getData('planets')} className="nav-category-btns">
             Planets
           </button>
         </NavLink>
         <NavLink to='/vehicles' className="nav-category-links">
-          <button onClick={() => getData('vehicles')}>
+          <button onClick={() => getData('vehicles')} className="nav-category-btns">
             Vehicles
           </button>
         </NavLink>
