63059aa06ee08a60b348c47f787421b6ef5fd4bd
Ophus Wong <ophus.wong@gmail.com>
2018-03-23 17:34:48 -0600

    Add styling to Nav for dropdown menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,23 +5,27 @@ import { func } from 'prop-types';
 
 const Nav = ({ getData }) => {
   return (
-    <nav>
-      <NavLink to='/people'>
-        <button onClick={() => getData('people')}>
-          People
-        </button>
-      </NavLink>
-      <NavLink to='/planets'>
-        <button onClick={() => getData('planets')}>
-          Planets
-        </button>
-      </NavLink>
-      <NavLink to='/vehicles'>
-        <button onClick={() => getData('vehicles')}>
-          Vehicles
-        </button>
-      </NavLink>
-    </nav>
+    <section className="nav-section">
+      <label for="reveal-email" class="btn"> Menu </label>
+      <input type="checkbox" id="reveal-email" role="button"></input>
+      <nav id="nav-category" className="categories">
+        <NavLink to='/people' className="nav-category-links">
+          <button onClick={() => getData('people')}>
+            People
+          </button>
+        </NavLink>
+        <NavLink to='/planets' className="nav-category-links">
+          <button onClick={() => getData('planets')}>
+            Planets
+          </button>
+        </NavLink>
+        <NavLink to='/vehicles' className="nav-category-links">
+          <button onClick={() => getData('vehicles')}>
+            Vehicles
+          </button>
+        </NavLink>
+      </nav>
+    </section>
   );
 };
 
